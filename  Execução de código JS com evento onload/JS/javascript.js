895f35fbfa4d3192217e6161f568e8d05adeb0bf
Anderson B Silva/ <32654298+oandersonbsilva@users.noreply.github.com>
2020-10-14 17:53:16 -0300

    Add files via upload

---
+++  Execução de código JS com evento onload/JS/javascript.js
@@ -0,0 +1,19 @@
+//declaração de váriaveis
+
+var nome = "Prof. Piter";
+var sobreNome= "Jordan";
+
+
+console.log("Instrução solta em um arquivo JS");
+
+//declaração de função
+function exibirNomeConsole(){
+    console.log("exibir nome" + nome);
+}
+
+function exibirNomeCompleto(){
+    console.log("Professor, nome completo" + nome + "" + sobreNome);
+}
+
+//chamadas de função
+exibirNomeConsole();
