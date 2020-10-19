6aec5d87a6c0b82a98fc799f13cdc960fd4d23b5
Anderson B Silva/ <32654298+oandersonbsilva@users.noreply.github.com>
2020-10-19 17:15:59 -0300

    Add files via upload

---
+++ Exemplo Programação Sincrona/JavaScript.js
@@ -0,0 +1,9 @@
+var curso= " Fromação JavaScript";
+
+console.log(" Olá Seja Bem o Vindo ao  Curso" + curso);
+
+for (let index =0; index < 10; index++) {
+    console.log(index);
+}
+
+console.log("Esse Curso é 10!!");
